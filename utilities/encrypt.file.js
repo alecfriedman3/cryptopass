e0b54a509e381eb26cffcb16cd71ea73cc0b4c2f
Alec Friedman <alec.friedman3@gmail.com>
2016-08-10 17:35:23 -0400

    encrypting files

--- utilities/encrypt.file.js
+++ utilities/encrypt.file.js
@@ -1,18 +1,30 @@
 var utils = require('./encrypt.utility.js')
 var encrypt = utils.encrypt;
 var decrypt = utils.decrypt;
+var fs = require('fs')
 var fileWriter = {}
 module.exports = fileWriter;
 
-fileWriter.validate = function (masterPw, secret, enSecret) {
+
+
+fileWriter.validate = function (masterPw) {
+	var secret = fs.readFileSync(__dirname + '/secret1.txt').toString();
+	var enSecret = fs.readFileSync(__dirname + '/secret2.txt').toString();
   try {
     var check = decrypt(enSecret, masterPw);
   } catch (error) {
+  	// console.error(error)
     return false;
   }
   return check === secret;
 }
 
-fileWriter.encryptFile = function () {}
+fileWriter.encryptFile = function (data, fileName, masterPswd) {
+
+	if (!fileWriter.validate(masterPswd)) throw new Error('Incorrect Master Password');
+	var encrypted = encrypt(JSON.stringify(data), masterPswd)
+	fs.writeFileSync(__dirname + '/' + fileName, encrypted)
+
+}
 
 fileWriter.decryptFile = function () {}
--- utilities/encrypt.file.test.js
+++ utilities/encrypt.file.test.js
@@ -2,10 +2,13 @@ let chai = require('chai')
 let expect = chai.expect;
 let fileWriter = require('./encrypt.file.js');
 let validate = fileWriter.validate;
+let encryptFile = fileWriter.encryptFile;
+let decryptFile = fileWriter.decryptFile;
 let utils = require('./encrypt.utility.js');
 let encrypt = utils.encrypt
 let decrypt = utils.decrypt
 let crypto = require('crypto');
+let fs = require('fs');
 
 describe('Encrypting and Decrypting Files', function (){
 
@@ -14,17 +17,58 @@ describe('Encrypting and Decrypting Files', function (){
     beforeEach('generate master passwords', function (){
       masterPassword = 'helloMyNameIsCrypto';
       falseMaster = "hello__?MyNameIsCrykslkfj9432to";
-      secret = "HelloIAmDogIDoge?";
-      ensecret = encrypt(secret, masterPassword);
+      secret = fs.readFileSync(__dirname + '/secret1.txt').toString()
+      fs.writeFileSync(__dirname + '/secret2.txt', encrypt(secret, masterPassword));
+    })
+
+    afterEach('delete encrypted secret', function (){
+    	fs.unlinkSync(__dirname + '/secret2.txt')
     })
 
     it('should validate the master password', function (){
-      expect(validate(masterPassword, secret, ensecret)).to.be.equal(true);
+      expect(validate(masterPassword)).to.be.equal(true);
     })
 
     it('should reject invalid master password', function (){
-      expect(validate(falseMaster, secret, ensecret)).to.be.equal(false);
+      expect(validate(falseMaster)).to.be.equal(false);
     })
+
+  })
+
+  describe('File Generation', function(){
+  	var data, fileName, masterPswd;
+  	beforeEach('write a file', function (){
+  		data = {
+  			Accounts: {
+  				Google: {
+  					username: 'IamDoge',
+  					password: 'dogedogedoge'
+  				}
+  			}
+  		}
+  		fileName = 'test.txt';
+  		masterPswd = "helloMyNameIsDoge"
+  		secret = fs.readFileSync(__dirname + '/secret1.txt').toString()
+      fs.writeFileSync(__dirname + '/secret2.txt', encrypt(secret, masterPswd));
+    })
+
+    afterEach('delete encrypted secret', function (){
+    	fs.unlinkSync(__dirname + '/secret2.txt')
+  		fs.unlinkSync(__dirname + '/' + fileName)
+    })
+
+  	it('should write to the filesystem', function (){
+  		encryptFile(data, fileName, masterPswd)
+  		expect(fs.readFileSync(__dirname + '/' + fileName)).to.be.ok
+  	})
+
+  	it('should encrypt the information', function (){
+  		encryptFile(data, fileName, masterPswd);
+  		var enData = fs.readFileSync(__dirname + '/' + fileName).toString()
+  		var decrypted = decrypt(enData, masterPswd);
+  		var newData = JSON.parse(decrypted);
+  		expect(newData).to.deep.equal(data)
+  	})
 
   })
 
