7c76a22347961b067dc2da1a622db399701b405f
Alec Friedman <alec.friedman3@gmail.com>
2016-08-12 12:57:08 -0400

    random passwords generating

--- utilities/password-utilities/pass.gen.js
+++ utilities/password-utilities/pass.gen.js
@@ -1,28 +1,26 @@
 var Random = require("random-js");
 var random = new Random(Random.engines.mt19937().autoSeed());
-var value = random.integer(1, 100)
-
 
 module.exports = {
 	createRandom: function (length, symTotal, numTotal){
 		var chars = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ"
 		var syms = "!@#$%^&*()_-+={[}]|\"';:.>,</?"
 		var nums = "0123456789"
-		var pass = new Array(length);
-		while (symTotal > 0 && pass.length !== length){
+		var pass = new Array(length).fill('');
+		while (symTotal > 0){
 			var ind = random.integer(0, length)
 			if (!pass[ind]){
 				pass[ind] = syms[random.integer(0, syms.length - 1)]
 				symTotal --
 			}
 		}
-		while (numTotal > 0 && pass.length !== length){
+		while (numTotal > 0){
 			var ind = random.integer(0, length)
-			if (!pass[ind]){
+			if (!pass[ind] && pass[ind] !== 0){
 				pass[ind] = nums[random.integer(0, nums.length - 1)]
 				numTotal --
 			}
 		}
-		return pass.fill(0).map(char => char ? char : chars[random.integer(0, chars.length - 1)]).join('')
+		return pass.map(char => char && char !== 0? char : chars[random.integer(0, chars.length - 1)]).join('')
 	}
 }
--- utilities/tests/pass.gen.test.js
+++ utilities/tests/pass.gen.test.js
@@ -10,7 +10,23 @@ describe('generating random passwords', function (){
 	})
 
 	it('should generate passwords with correct number of symbols', function (){
-		expect(createRandom(20, 3).split().filter(elem => elem.match(new RegExp("!@#$%^&*()_-+={[}]|\"';:.>,</?")))).to.have.length(3)
+		var random = createRandom(20, 3)
+		random = random.split('').filter(elem => elem.match(/\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\_|\-|\+|\=|\{|\[|\}|\]|\||\"|\'|\;|\:|\.|\>|\,|\<|\/|\?/))
+		expect(random).to.have.length(3)
+	})
+
+	it('should generate passwords with correct number of numbers', function (){
+		var random = createRandom(20, 0, 5)
+		random = random.split('').filter(elem => elem.match(/[0-9]/))
+		expect(random).to.have.length(5)
+	})
+
+	it('should generate passwords with correct number of numbers and symbols', function (){
+		var random = createRandom(20, 8, 5)
+		randomNums = random.split('').filter(elem => elem.match(/[0-9]/))
+		randomSyms = random.split('').filter(elem => elem.match(/\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\_|\-|\+|\=|\{|\[|\}|\]|\||\"|\'|\;|\:|\.|\>|\,|\<|\/|\?/))
+		expect(randomNums).to.have.length(5)
+		expect(randomSyms).to.have.length(8)
 	})
 
 })
