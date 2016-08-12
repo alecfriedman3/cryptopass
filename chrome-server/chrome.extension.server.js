13b911067465e25716245e8fb0518e18ceea0d0c
Alec Friedman <alec.friedman3@gmail.com>
2016-08-12 16:37:56 -0400

    sockets setup serverside

--- chrome-server/chrome.extension.server.js
+++ chrome-server/chrome.extension.server.js
@@ -2,16 +2,35 @@
 var http = require('http');
 var app = require('express')();
 var chalk = require('chalk');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
 
 var server = http.createServer(app)
 var io = require('socket.io')(server)
 
 io.on('connection', function (socket){
 
-	socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  socket.on('addFromChrome', function (data) {
+  	// get the encrypted data from chrome extension and write it to the fs
+  	fs.writeFileAsync(__dirname + '/../utilities/data.txt', data.data)
+  	.then(function (){
+  		// emmit message to electron telling it to update the local data in app
+    	socket.emit('chromeAdd')
+  	}).catch(console.error.bind(console))
   });
+
+  socket.on('addFromElectron', function (data){
+  	// get the encrypted data from electron app and write it to the fs
+  	fs.writeFileAync(__dirname + '/../utilities/data.txt', data.data)
+  	.then(function (){
+  		// read the newly encrypted file
+  		return fs.readFileSync(__dirname + '/../utilities/data.txt')
+  	})
+  	.then(function (file){
+  		// send the newly encrypted file back to chrome extension
+    	socket.emit('electronAdd', file.toString())
+  	}).catch(console.error.bind(console))
+  })
 
 })
 
