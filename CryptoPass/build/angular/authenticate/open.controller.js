b70e49063f3a1554b3cf38385efaedf0e3296d88
Reed Branson <reed.branson@gmail.com>
2016-08-15 15:43:03 -0400

    janky working version

--- CryptoPass/build/angular/authenticate/open.controller.js
+++ CryptoPass/build/angular/authenticate/open.controller.js
@@ -1,8 +1,7 @@
 app.controller('openController', function($scope, $state, $rootScope){
 
-  settings.get('userds').then(val => {
+  settings.get('user').then(val => {
     if (val) {
-      // console.log(val);
       $state.go('auth');
     } else {
       $state.go('firstLogin');
