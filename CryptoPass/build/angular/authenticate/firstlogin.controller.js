4e8139a6f08c5981b6bce74d3692ac8acc6fc220
Reed Branson <reed.branson@gmail.com>
2016-08-15 16:03:39 -0400

    fixed issues

--- CryptoPass/build/angular/authenticate/firstlogin.controller.js
+++ CryptoPass/build/angular/authenticate/firstlogin.controller.js
@@ -6,10 +6,12 @@ app.controller('firstLoginController', function($scope, $state, $rootScope){
   $scope.master = null;
 
   $scope.setPassword = function (master){
-    if ($scope.master === $scope.master2) {
+    if ($scope.master === $scope.master2 && $scope.master2 === $scope.master3) {
       utils.generateSecret(master);
-      utils.encryptFile({}, master);
+      utils.encryptFile({login: [], creditCard: [], identity: [], note: [] }, master);
       settings.set('user', true).then(() => {
+        masterPass = master;
+        masterObj = {login: [], creditCard: [], identity: [], note: [] };
         $rootScope.validated = true;
         $rootScope.$evalAsync()
         $state.go('home')
