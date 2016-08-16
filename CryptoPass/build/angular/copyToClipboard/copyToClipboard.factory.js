7e18e4e58573069db9771fcfc4bce1ed956eb2a7
Elliott McNary <elliottmcnary@gmail.com>
2016-08-16 15:13:51 -0400

    highlightable class and copy btn

---
+++ CryptoPass/build/angular/copyToClipboard/copyToClipboard.factory.js
@@ -0,0 +1,11 @@
+var remote = require('electron').remote;
+var clipboard = remote.clipboard;
+
+app.factory('Clipboard', function(){
+  return {
+    copy: function(text){
+      console.log('copy invoked');
+      clipboard.writeText(text)
+    }
+  }
+})
