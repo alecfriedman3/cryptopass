ff2a9af01bac10abd02de631afe486d9a4da0bdd
Elliott McNary <elliottmcnary@gmail.com>
2016-08-16 19:55:22 -0400

    shit working

---
+++ myApp/www/angular/app.js
@@ -0,0 +1,84 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('cryptoPass', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.login', {
+    url: '/login',
+    views: {
+      'menuContent': {
+        templateUrl: 'angular/login/login.view.html',
+        controller: 'loginController'
+      }
+    }
+  })
+  .state('app.creditCard', {
+    url: '/login',
+    views: {
+      'menuContent': {
+        templateUrl: 'angular/creditCard/creditCards.all.view.html',
+        controller: 'creditCardController'
+      }
+    }
+  })
+
+  .state('app.identity', {
+      url: '/identity',
+      views: {
+        'menuContent': {
+          templateUrl: 'angular/identity/identity.all.view.html',
+          controller: 'identityController'
+        }
+      }
+    })
+    .state('app.note', {
+      url: '/note',
+      views: {
+        'menuContent': {
+          templateUrl: 'angular/note/note.all.view.html',
+          controller: 'noteController'
+        }
+      }
+    })
+
+  .state('app.settings', {
+    url: '/settings',
+    views: {
+      'menuContent': {
+        templateUrl: 'angular/settings/settings.view.html',
+        controller: 'settingsController'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/login');
+});
