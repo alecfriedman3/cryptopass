4bfada73ab27f341569fdf00162604f2d7c584b3
Julia Siwak <JuliaNYC@users.noreply.github.com>
2016-08-17 17:53:36 -0400

    ui.slider works now

--- myApp/www/angular/authenticate/auth.controller.js
+++ myApp/www/angular/authenticate/auth.controller.js
@@ -1,6 +1,5 @@
 app.controller('authController', function($scope, $state){
-	// $scope.master = null;
-	$scope.checkMaster=function(master){
+		$scope.checkMaster=function(master){
 		console.log(master)
 		$state.go('app.home');
 	}
--- myApp/www/angular/login/login.controller.js
+++ myApp/www/angular/login/login.controller.js
@@ -12,6 +12,30 @@ app.controller('loginSingleController', function($stateParams, $scope, $state){
 
 
 app.controller('addLoginController', function($scope, $state){
+		$scope.login = {
+		name: null,
+		username: null,
+		password: null
+	}
+	$scope.gen = null
 
+	$scope.generate = function (){
+		$scope.gen = !$scope.gen
+	}
+
+	$scope.generatePassword = function (len, syms, nums){
+		$scope.login.password = createRandom(+len, +syms, +nums)
+
+	}
+
+	$scope.createLogin = function (){
+		var newId = masterObj.login.length ? masterObj.login[masterObj.login.length - 1].id + 1 : 1;
+		$scope.login.id = newId
+		masterObj.login.push($scope.login)
+		var encrypted = encrypt(JSON.stringify(masterObj), masterPass)
+		socket.emit('addFromElectron', {data: encrypted})
+		$rootScope.$evalAsync()
+		$state.go('login.single', {id: newId}, {reload: true})
+	}
 
 })
