2218d93873944bbd1cbd380a26076d36b713d8f4
Raekwon <RaekwonStorm@users.noreply.github.com>
2016-08-17 09:30:51 -0400

    Update encrypt.file.js

--- CryptoPass/utilities/encrypt.file.js
+++ CryptoPass/utilities/encrypt.file.js
@@ -5,7 +5,7 @@ var decrypt = utils.decrypt;
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 var fileWriter = {};
-// var settings = require('electron-settings');
+var settings = require('electron-settings');
 var mkdirp = require('mkdirp-promise')
 module.exports = fileWriter;
 
@@ -25,31 +25,31 @@ fileWriter.encryptFile = function (data, masterPswd) {
 	// upon exiting application, encrypt data and write to file
 	var encrypted = encrypt(JSON.stringify(data), masterPswd)
 	return fs.writeFileAsync(__dirname + '/data.txt', encrypted)
-	// .then(() => {
-	// 	return settings.get('dropboxPath')
-	// })
-	// .then(val => {
-	// 	if(val) {
-	// 		//Use Stat to check if file exists (fs.exists is deprecated)
-	// 		return fs.statAsync(val + '/Apps/CryptoPass').then((stat) => {
-	// 			if(!err) {
-	// 				//If No error then it exists.  Write that data
-	// 				return fs.writeFileAsync(val + '/Apps/CryptoPass/data.txt', encrypted);
-	// 			}
-	// 		})
-	// 		//write secret if we don't get an error
-	// 		.then(() => fs.writeFileAsync(val + '/Apps/CryptoPass/secret2.txt', fileWriter.generateSecret(masterPswd)))
-	// 			//Need to catch the error from stat Async and then make directory and writefile
-	// 		//This should only happen first time user connects dropbox
-	// 		.catch(err => {
-	// 			//recursively create dir if it doesn't exist
-	// 			return mkdirp(val + '/Apps/CryptoPass')
-	// 		})
-	// 		//after we make dir write the data to dir
-	// 		.then(() => fs.writeFileAsync(val + '/Apps/CryptoPass/data.txt', encrypted))
-	// 		.then(() => fs.writeFileAsync(val + '/Apps/CryptoPass/secret2.txt', fileWriter.generateSecret(masterPswd)))
-	// 	}
-	// })
+	.then(() => {
+		return settings.get('dropboxPath')
+	})
+	.then(val => {
+		if(val) {
+			//Use Stat to check if file exists (fs.exists is deprecated)
+			return fs.statAsync(val + '/Apps/CryptoPass').then((stat) => {
+				if(!err) {
+					//If No error then it exists.  Write that data
+					return fs.writeFileAsync(val + '/Apps/CryptoPass/data.txt', encrypted);
+				}
+			})
+			//write secret if we don't get an error
+			.then(() => fs.writeFileAsync(val + '/Apps/CryptoPass/secret2.txt', fileWriter.generateSecret(masterPswd)))
+				//Need to catch the error from stat Async and then make directory and writefile
+			//This should only happen first time user connects dropbox
+			.catch(err => {
+				//recursively create dir if it doesn't exist
+				return mkdirp(val + '/Apps/CryptoPass')
+			})
+			//after we make dir write the data to dir
+			.then(() => fs.writeFileAsync(val + '/Apps/CryptoPass/data.txt', encrypted))
+			.then(() => fs.writeFileAsync(val + '/Apps/CryptoPass/secret2.txt', fileWriter.generateSecret(masterPswd)))
+		}
+	})
 	.catch(err => console.error(err))
 
 }
