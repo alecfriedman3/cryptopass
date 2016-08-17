8e9ebf9b07a0789b8eb5fa81b394cf632647febb
Alec Friedman <alec.friedman3@gmail.com>
2016-08-17 09:25:11 -0400

    console.log removed

--- CryptoPass/build/angular/copyToClipboard/copyToClipboard.factory.js
+++ CryptoPass/build/angular/copyToClipboard/copyToClipboard.factory.js
@@ -4,7 +4,6 @@ var clipboard = remote.clipboard;
 app.factory('Clipboard', function(){
   return {
     copy: function(text){
-      console.log('copy invoked');
       clipboard.writeText(text)
     }
   }
