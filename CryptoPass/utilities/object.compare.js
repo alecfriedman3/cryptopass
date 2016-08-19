0a2159f4575290ea962a8fd07682d586e8d6c001
Alec Friedman <alec.friedman3@gmail.com>
2016-08-19 15:26:32 -0400

    compare and delete

---
+++ CryptoPass/utilities/object.compare.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+  compareAndMerge: function (merger, base){
+    for(var key in merger){
+      for (var i = 0; i < merger[key].length; i++){
+        // if the base object does not contain the merge, and the merge is not deleted, add the merge
+        if (!base[key].filter(obj => obj.id == merger[key][i].id).length /*&& !merger[key][i].deleted*/){
+          base[key].push(merger[key][i])
+          continue
+        }
+        for (var j = 0; j < base[key].length; j++){
+          if (base[key][j].name == merger[key][i].name && merger[key][i].id == base[key][j].id /*&& !merger[key][i].deleted*/){
+            // if the merging object was updated more frequently, overwrite it in the base
+            if (merger[key][i].lastUpdated > base[key][j].lastUpdated){
+              base[key][j] = JSON.parse(JSON.stringify(merger[key][i]))
+            }
+          }
+        }
+      }
+    }
+    return base
+  },
+  compareAndDelete: function (merger, base){
+    for (var key in merger){
+      var keysToCheck = Object.keys(merger[key][0])
+      for (var i = 0; i < merger[key].length; i++){
+        for (var j = 0; j < base[key].length; j++){
+          if (merger[key][i].deleted && base[key][j].name == merger[key][i].name && merger[key][i].id == base[key][j].id && merger[key][i].lastUpdated > base[key][j].lastUpdated){
+            base[key].splice(j, 1)
+          }
+        }
+      }
+    }
+    return base
+  }
+}
+
