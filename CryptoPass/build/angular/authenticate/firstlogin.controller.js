74978c0d5f2a901db256fa9782699bfd3faeec3c
Julia Siwak <JuliaNYC@users.noreply.github.com>
2016-08-19 14:33:21 -0400

    delete confirm works

--- CryptoPass/build/angular/authenticate/firstlogin.controller.js
+++ CryptoPass/build/angular/authenticate/firstlogin.controller.js
@@ -8,15 +8,11 @@ app.controller('firstLoginController', function($scope, $state, $rootScope){
   $scope.master = null;
 
   $scope.setPassword = function (master){
-    // var string=$scope.master.toString();
-    // console.log(string.length);
-
-    console.log($scope.master.length)
     if ($scope.master.length<6 && $scope.master2.length<6 && $scope.master3.length<6){
       alert("Hey! We want your password to be a little more secure! Please choose a password with at least 6 characters");
-      console.log('helloooo');
-      console.log($scope.master.length);
+
     }
+
     else {
       if ($scope.master === $scope.master2 && $scope.master2 === $scope.master3) {
       utils.generateSecret(master);
@@ -33,25 +29,6 @@ app.controller('firstLoginController', function($scope, $state, $rootScope){
     }
   }
     }
-    // else {
-    // if ($scope.master === $scope.master2 && $scope.master2 === $scope.master3) {
-    //   utils.generateSecret(master);
-    //   utils.encryptFile({login: [], creditCard: [], identity: [], note: [] }, master);
-    //   settings.set('user', true).then(() => {
-    //     masterPass = master;
-    //     masterObj = {login: [], creditCard: [], identity: [], note: [] };
-    //     $rootScope.validated = true;
-    //     $rootScope.$evalAsync()
-    //     $state.go('home')
-    //   })
-    // } else {
-    //   alert ("Your Passwords Do Not Match!");
-    // }
-  // }
-  // else {
-  //   alert("All good");
-  // }
-  // }
 
   $scope.username;
 
