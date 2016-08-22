4104463182602bd615aed69d36f44871721213e4
Alec Friedman <alec.friedman3@gmail.com>
2016-08-22 18:04:37 -0400

    blah nothing

--- myApp/www/angular/utilities/classified/hashingBackup.js
+++ myApp/www/angular/utilities/classified/hashingBackup.js
@@ -2,10 +2,10 @@ var crypto = require('crypto-js/');
 
 module.exports = {
   scramble: function(){
-    let uuid = device.uuid.split(''); //eslint-disable-line
-    let serial = device.serial.split(''); //eslint-disable-line
-    let array = uuid.concat(serial);
-    let key = '';
+    var uuid = device.uuid.split(''); //eslint-disable-line
+    var serial = device.serial.split(''); //eslint-disable-line
+    var array = uuid.concat(serial);
+    var key = '';
     array.forEach(function(str){
       if(parseInt(str)) str = str * 3;
   	  else str += 'c'
