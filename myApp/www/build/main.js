b93681fc78eb4ebdc197c155ab03765e43dc6110
Julia Siwak <JuliaNYC@users.noreply.github.com>
2016-08-23 14:10:46 -0400

    add icons folder

--- myApp/www/build/main.js
+++ myApp/www/build/main.js
@@ -17892,15 +17892,7 @@ app.controller('addIdentityController', function($scope, $state, $stateParams, $
   	data: null
   }
 
-  // $scope.createId = function() {
-  //   var newId = masterObj.identity.length ? masterObj.identity[masterObj.identity.length - 1].id + 1 : 1
-  //   $scope.identity.id = newId
-  //   if ($scope.identity) masterObj.identity.push($scope.identity)
-  //   var encrypted = encrypt(JSON.stringify(masterObj), masterPass)
-  //   socket.emit('addFromElectron', { data: encrypted })
-  //   $rootScope.$evalAsync()
-  //   $state.go('identity.single', { id: newId }, {reload: true})
-  // }
+
 
 })
 app.controller('loginController', function($scope, $state){
